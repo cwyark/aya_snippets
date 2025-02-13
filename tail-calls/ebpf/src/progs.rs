fe99e48e5171ed3fa21b3827c7298698c50df208
cwyark <chester@cwyark.me>
2025-02-13 17:01:45 +0800

    add prog2 and prog3

--- tail-calls/ebpf/src/progs.rs
+++ tail-calls/ebpf/src/progs.rs
@@ -39,6 +39,72 @@ unsafe fn try_prog1(ctx: BtfTracePointContext) -> Result<u32, i64> {
     if res.is_err() {
         error!(&ctx, "prog1: tail_call failed");
     }
+    Ok(0)
+}
+
+pub fn prog2(ctx: BtfTracePointContext) -> u32 {
+    match unsafe { try_prog2(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret.try_into().unwrap(),
+    }
+}
+
+unsafe fn try_prog2(ctx: BtfTracePointContext) -> Result<u32, i64> {
+    let cpu_id = bpf_get_smp_processor_id();
+    let tgid = (bpf_get_current_pid_tgid() >> 32) as u32;
+
+    let shared_state = unsafe {
+        let ptr = maps::PER_CPU_MAP.get_ptr_mut(0).ok_or(0)?;
+        &mut *ptr
+    };
+
+    shared_state.per_event_count += 1;
+    shared_state.total_count += 1;
+
+    info!(
+        &ctx,
+        "[prog2] cpu_id: {}, tgid: {}, per_event_count: {}, total_count: {}",
+        cpu_id,
+        tgid,
+        shared_state.per_event_count,
+        shared_state.total_count
+    );
+
+    let res = maps::TAIL_CALL_MAP.tail_call(&ctx, 1);
+    if res.is_err() {
+        error!(&ctx, "prog2: tail_call failed");
+    }
+
+    Ok(0)
+}
+
+#[btf_tracepoint(function = "prog3")]
+pub fn prog3(ctx: BtfTracePointContext) -> u32 {
+    match unsafe { try_prog3(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret.try_into().unwrap(),
+    }
+}
+
+unsafe fn try_prog3(ctx: BtfTracePointContext) -> Result<u32, i64> {
+    let cpu_id = bpf_get_smp_processor_id();
+    let tgid = (bpf_get_current_pid_tgid() >> 32) as u32;
+
+    let shared_state = unsafe {
+        let ptr = maps::PER_CPU_MAP.get_ptr_mut(0).ok_or(0)?;
+        &mut *ptr
+    };
+    shared_state.per_event_count += 1;
+    shared_state.total_count += 1;
+
+    info!(
+        &ctx,
+        "[prog3] cpu_id: {}, tgid: {}, per_event_count: {}, total_count: {}",
+        cpu_id,
+        tgid,
+        shared_state.per_event_count,
+        shared_state.total_count
+    );
 
     Ok(0)
 }
